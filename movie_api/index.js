58e9264f61c249abe25bdf28a52a952db4b37d75
mayjessican <63651624+mayjessican@users.noreply.github.com>
2020-07-07 16:39:35 +0200

    task 2.4

--- movie_api/index.js
+++ movie_api/index.js
@@ -3,4 +3,26 @@ const morgan = require("morgan");
 const app = express();
 
 //Middleware
+app.use(express.static('public/documentation.html'))
 app.use(morgan("common"));
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// GET requests
+app.get('/', (req, res) => {
+  res.send('Welcome to myFlix!');
+});
+
+app.get('/documentation', (req, res) => {
+  res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+app.get('/movies', (req, res) => {
+  res.json(movies);
+});
+
+// listen for requests
+app.listen(8080, function(){ console.log('Server listening on 8080...')});
