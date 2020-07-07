9d3609c625c7b7769980ab3d8cadb620f0b8d65a
mayjessican <63651624+mayjessican@users.noreply.github.com>
2020-07-07 11:39:18 +0200

    added index.html and updated coding for task 2.2

---
+++ movie_api/index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+//Middleware
+app.use(morgan("common"));
--- movie_api/server.js
+++ movie_api/server.js
@@ -3,21 +3,31 @@ fs = require('fs'),
 url = require('url');
 
 http.createServer((request, response) => {
-  var addr = request.url,
-  q = url.parse(addr, true),
-  filePath = '';
+    var addr = request.url,
+        q = url.parse(addr, true),
+        filePath = '';
 
-  if (q.pathname.includes('documentation')) {
-    filePath = (__dirname + '/documentation.html');
-  } else {
-    filePath = 'index.html';
-  }
-
-  fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', function(err) {
-    if (err) {
-      console.log(err);
+    if (q.pathname.includes('documentation')) {
+        filePath = (__dirname + '/public/documentation.html');
     } else {
-      console.log('Added to log.');
+        filePath = 'index.html';
     }
-  })
-}).listen(8080)
+
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        throw err;
+    }
+
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(data);
+    response.end();
+
+    fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Added to log.');
+        }
+    });
+
+}).listen(8080, function(){ alert('Server listening on 8080...')});
