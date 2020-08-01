a8b0d9932424488c284014ac033134f62807ce44
mayjessican <63651624+mayjessican@users.noreply.github.com>
2020-08-01 17:56:55 +0200

    updated for task 2.8

--- movie_api/index.js
+++ movie_api/index.js
@@ -3,6 +3,13 @@ const express = require("express"),
   uuid = require("uuid");
 const morgan = require("morgan");
 const app = express();
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+
+const Movies = Models.Movie;
+const Users = Models.User;
+
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
 
 //Middleware
 app.use(express.static('public/documentation.html'));
@@ -10,62 +17,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json());
 
 let favourite = [];
-
-let movies = [
-  {id: 9,
-  title: "Pans Labyrinth",
-  description: "In 1944 Spain, a lonely girl encounters a faun in an ancient maze and must complete three dangerous tasks to achieve immortality.",
-  genre: "Science Fiction",
-  director: "Guillermo del Toro"},
-
-  {id: 8,
-  title: "Jaws",
-  description: "A giant shark is feeding on the good people of Amity Island and it's up to police chief Martin Brody to hunt down the finned fiend.",
-  genre: "Horror",
-  director: "Steven Spielberg"},
-
-  {id: 7,
-  title: "Shrek",
-  description: "A grouchy ogre and his jabbering donkey sidekick set out to rescue an imprisoned princess from a scheming lord",
-  genre: "Adventure",
-  director: "Andrew Adamson"},
-
-  {id: 6,
-  title: "The last of the Mohicans",
-  description: "The Last of the Mohicans is a 1992 American epic historical drama film set in 1757 during the French and Indian War.",
-  genre: "Action",
-  director: "Michael Mann"},
-
-  {id: 5,
-  title: "Out of Africa",
-  description: "Based on the story of Karen Blixen, who leaves her native Denmark in 1913 to marry a baron and run a coffee plantation in Kenya.",
-  genre: "Biography",
-  director: "Sydney Pollock"},
-
-  {id: 4,
-  title: "Waiting to exhale",
-  description: "The story centers on four women living in the Phoenix, Arizona area and their relationships with men and one another. All of them are holding their breath until the day they can feel comfortable in a committed relationship with a man.",
-  genre: "Comedy",
-  director: "Forest Whitaker"},
-
-  {id: 3,
-  title: "Parasite",
-  description: "Greed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan",
-  genre: "Comedy",
-  director: "Bong Joon-ho"},
-
-  {id: 2,
-  title: "Black Panther",
-  description: "After his father's tragic death, and the events involving members of the Avengers that followed, T'Challa returned to Wakanda as King",
-  genre: "Action",
-  director: "Ryan Coogler"},
-
-  {id: 1,
-  title: "The Godfather",
-  description: "The Godfather Don Vito Corleone is the head of the Corleone mafia family in New York. He is at the event of his daughter's wedding. Michael, Vito's youngest son and a decorated WW II Marine is also present at the wedding. Michael seems to be uninterested in being a part of the family business.",
-  genre: "Crime",
-  director: "Francis Ford Coppola"}
-];
+let movies = [];
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
@@ -82,75 +34,194 @@ app.get('/documentation', (req, res) => {
   res.sendFile('public/documentation.html', { root: __dirname });
 });
 
+// Gets the list of data about ALL movies
+app.get('/movies', (req, res) => {
+  Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
 // Gets the data about a movie title
 app.get('/movies/:title', (req, res) => {
-  res.json(movies.find((movies) =>
-    {return movies.title === req.params.title}));
+  Movies.findOne({ Title: req.params.title })
+    .then((movie) => {
+      res.json(movie);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
 // Get the genre about a movie
+// app.get('/genres/:name', (req, res) => {
+//   const results = movies.find((movie) =>
+//     {return movie.genre === req.params.name});
+//   res.json(results);
+// });
 app.get('/genres/:name', (req, res) => {
-  const results = movies.find((movie) =>
-    {return movie.genre === req.params.name});
-  res.json(results);
-});
-
-// Gets the list of data about ALL movies
-app.get('/movies', (req, res) => {
-  res.json(movies);
+  Users.findOne({ Genre: req.params.name })
+    .then((movie) => {
+      res.json(movie);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
 // Gets the data about a movie director
-app.get('/directors/:name', (req, res) => {
-  res.json(movies.find((movie) =>
-    {return movie.director === req.params.name}));
+// app.get('/directors/:name', (req, res) => {
+//   res.json(movies.find((movie) =>
+//     {return movie.director === req.params.name}));
+// });
+app.get('/director/:name', (req, res) => {
+  Movies.findOne({ Director: req.params.name })
+    .then((movies) => {
+      res.json(
+        "Name: " + movies.Director.Name +
+        "Bio: " + movies.Director.Bio +
+        "Birthday: " + movies.Director.Birthday
+      );
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
 let users = []
 
 //Gets data about users
+// app.get('/users', (req, res) => {
+//   res.json(users);
+// });
+
+// Get all users
 app.get('/users', (req, res) => {
-  res.json(users);
+  Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
-// Adds data for a new users
+//Add a user
+/* We’ll expect JSON in this format
+{
+  ID: Integer,
+  Username: String,
+  Password: String,
+  Email: String,
+  Birthday: Date
+}*/
 app.post('/users', (req, res) => {
-  let newUser = req.body;
-  if(!newUser.name){
-    const message = 'Missing name in request body';
-    res.status(400).send (message);
-  } else {
-    newUser.id = uuid.v4();
-    users.push(newUser);
-    res.status(200).send(newUser);
-  }
+  Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + ' already exists');
+      } else {
+        Users
+          .create({
+            Username: req.body.Username,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+          .then((user) =>{res.status(201).json(user) })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send('Error: ' + error);
+        })
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
 });
 
-// Updates user information
-app.put('/users/:id', (req, res) => {
-  let userName = req.params.name;
-  users = users.find((user_id) => { return user_id !== req.params.id });
-  res.status(200).send('User ' + req.params.id + ' was updated.');
+// Get a user by username
+app.get('/users/:Username', (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
-// Deletes a user from our list by ID
-app.delete('/users/:id', (req, res) => {
-  let user_id = req.params.id;
-     users = users.filter((user_id) => { return user_id !== req.params.id });
-     res.status(200).send('User ' + req.params.id + ' was deleted.');
-  // }
+// Update a user's info, by username
+/* We’ll expect JSON in this format
+{
+  Username: String,
+  (required)
+  Password: String,
+  (required)
+  Email: String,
+  (required)
+  Birthday: Date
+}*/
+app.put('/users/:Username', (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
+    {
+      Username: req.body.Username,
+      Password: req.body.Password,
+      Email: req.body.Email,
+      Birthday: req.body.Birthday
+    }
+  },
+  { new: true }, // This line makes sure that the updated document is returned
+  (err, updatedUser) => {
+    if(err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
 });
 
-// Updates user favourites
-app.post('/users/:name/movies/:movie_id', (req, res) => {
-  let movie_id = req.params.movie_id;
-  let userName = req.params.name;
-  if (userName) {
-  favourite = favourite.push(movie_id);
-    res.status(200).send('Movie ' + req.params.movie_id + ' was assigned to favourites.');
-  } else {
-    res.status(404).send('Movie ' + req.params.movie_id + ' was not found.');
-  }
+// Delete a user by username
+app.delete('/users/:Username', (req, res) => {
+  Users.findOneAndRemove({ Username: req.params.Username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + ' was not found');
+      } else {
+        res.status(200).send(req.params.Username + ' was deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+// Add a movie to a user's list of favorites
+app.post('/users/:Username/Movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, {
+     $push: { FavoriteMovies: req.params.MovieID }
+   },
+   { new: true }, // This line makes sure that the updated document is returned
+  (err, updatedUser) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
 });
 
 //Deletes a movie from user favourites
