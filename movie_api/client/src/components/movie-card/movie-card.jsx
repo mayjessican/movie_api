d1c0e19bd13973323373cd3e5012837b37c28efc
mayjessican <63651624+mayjessican@users.noreply.github.com>
2020-09-01 13:50:49 +0200

    fixed prop warnings for task 3.4

--- movie_api/client/src/components/movie-card/movie-card.jsx
+++ movie_api/client/src/components/movie-card/movie-card.jsx
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 
 export class MovieCard extends React.Component {
   render() {
-    const { movie, onClick } = this.props;
+    const { movie, onclick } = this.props;
 
     return (
       <Card style={{ width: '16rem' }}>
@@ -34,7 +34,7 @@ MovieCard.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
-    ImagePath: PropTypes.string.isRequired
+    ImageURL: PropTypes.string.isRequired
   }).isRequired,
-  onClick: PropTypes.func.isRequired
+  //onClick: PropTypes.func.isRequired
 };
--- movie_api/client/src/components/movie-view/movie-view.jsx
+++ movie_api/client/src/components/movie-view/movie-view.jsx
@@ -63,7 +63,7 @@ MovieView.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
-    ImagePath: PropTypes.string.isRequired,
+    ImageURL: PropTypes.string.isRequired,
     Genre: PropTypes.shape({
       Name: PropTypes.string.isRequired,
     }),
